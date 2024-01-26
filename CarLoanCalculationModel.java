5400fbe4ec3ded37671f1191cfe3f8f013355de2
iZ86 <iZLikesCS@gmail.com>
2024-01-26 08:40:47 +0800

    In CarLoanCalculationModel Class, added comments for every method, and added calculateLoan method

--- CarLoanCalculationModel.java
+++ CarLoanCalculationModel.java
@@ -2,67 +2,101 @@
 /** This class represents the calculation model for the Kawaguchi bank car loan program. */
 public class CarLoanCalculationModel {
     private boolean loanInsurance;
-    private double totalLoanYears, totalLoanAmount, loanPayback, interestRate, monthlyRepayment;
+    private double loanYears, loanAmount, loanPayback, interestRate, monthlyRepayment;
     private String carType;
 
     public CarLoanCalculationModel() {
         this.loanInsurance = false;
-        this.totalLoanYears = this.totalLoanAmount = this.loanPayback = this.interestRate = this.monthlyRepayment = 0;
+        this.loanYears = this.loanAmount = this.loanPayback = this.interestRate = this.monthlyRepayment = 0;
         this.carType = "None";
     }
 
+    /** Calculates the loan and saves the data. */
+    public void calculateLoan(String carType, double loanYears, double loanAmount, double interestRate) {
+        this.carType = carType;
+        this.loanYears = loanYears;
+        this.loanAmount = loanAmount;
+        this.interestRate = interestRate / 100;
+        this.loanPayback = roundUpTwoDecimal(loanAmount + (loanYears * loanAmount * interestRate));
+        this.monthlyRepayment = roundUpTwoDecimal((this.loanPayback / loanYears) / 12);
+    }
+
+    /** Rounds up any value to two decimal places. */
+    private double roundUpTwoDecimal(double number) {
+        return (double) (Math.round(number * 100)) / 100;
+    }
+
+    /** Returns true if there is loan insurance.
+     * Otherwise, false
+     */
     public boolean isLoanInsurance() {
         return loanInsurance;
     }
 
+    /** Sets the loan insurance,
+     * true if it there is loan insurance.
+     * Otherwise, false
+     */
     public void setLoanInsurance(boolean loanInsurance) {
         this.loanInsurance = loanInsurance;
     }
 
-    public double getTotalLoanYears() {
-        return totalLoanYears;
+    /** Returns the loan years. */
+    public double getloanYears() {
+        return loanYears;
     }
 
-    public void setTotalLoanYears(double totalLoanYears) {
-        this.totalLoanYears = totalLoanYears;
+    /** Sets the loan years. */
+    public void setloanYears(double loanYears) {
+        this.loanYears = loanYears;
     }
 
-    public double getTotalLoanAmount() {
-        return totalLoanAmount;
+    /** Returns the loan amount. */
+    public double getLoanAmount() {
+        return this.loanAmount;
     }
 
-    public void setTotalLoanAmount(double totalLoanAmount) {
-        this.totalLoanAmount = totalLoanAmount;
+    /** Sets the loan amount. */
+    public void setLoanAmount(double loanAmount) {
+        this.loanAmount = loanAmount;
     }
 
+    /** Returns the loan payback. */
     public double getLoanPayback() {
         return loanPayback;
     }
 
+    /** Sets the loan payback. */
     public void setLoanPayback(double loanPayback) {
         this.loanPayback = loanPayback;
     }
 
+    /** Returns the interest rate. */
     public double getInterestRate() {
         return interestRate;
     }
 
+    /** Sets the interest rate. */
     public void setInterestRate(double interestRate) {
         this.interestRate = interestRate;
     }
 
+    /** Returns the monthly payment. */
     public double getMonthlyRepayment() {
         return monthlyRepayment;
     }
 
+    /** Sets the monthly payment. */
     public void setMonthlyRepayment(double monthlyRepayment) {
         this.monthlyRepayment = monthlyRepayment;
     }
 
+    /** Returns the car type. */
     public String getCarType() {
         return carType;
     }
 
+    /** Sets the car type. */
     public void setCarType(String carType) {
         this.carType = carType;
     }
