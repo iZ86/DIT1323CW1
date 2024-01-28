e4af7e54cd0586cd7977ac0e03c69ffcd7df258d
iZ86 <iZLikesCS@gmail.com>
2024-01-28 15:41:59 +0800

    Added starter code for MainMenuView Class

--- MainMenuView.java
+++ MainMenuView.java
@@ -3,15 +3,17 @@ import javax.swing.border.CompoundBorder;
 import java.awt.*;
 
 /** This class represents the kawaguchi bank car loan main menu. */
+// My goal for every main class, is to toss a panel object to the controller.
+// Which the controller will just change using the cardLayout.
 public class MainMenuView {
-    private JFrame mainMenuFrame;
+    private JPanel mainMenuPanel;
 
     public MainMenuView() {
-        this.mainMenuFrame = setupMainMenuViewFrame();
+        this.mainMenuPanel = setupMainMenuViewPanel();
         
     }
 
-    private JFrame setupMainMenuViewFrame() {
+    private JPanel setupMainMenuViewPanel() {
         return null;
     }
 }
