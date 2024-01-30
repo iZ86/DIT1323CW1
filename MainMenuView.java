4192fffdea120800305db2ccfee3c70190237cab
iZLikesCS@gmail.com <iZLikesCS@gmail.com>
2024-01-30 17:59:56 +0800

    Modified to be a JPanel instead of JFrame, add the title of the main menu for now

--- MainMenuView.java
+++ MainMenuView.java
@@ -1,17 +1,40 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import javax.swing.border.CompoundBorder;
 import java.awt.*;
 
 /** This class represents the kawaguchi bank car loan main menu. */
 public class MainMenuView {
-    private JFrame mainMenuFrame;
+    private JPanel mainMenuViewPanel;
 
     public MainMenuView() {
-        this.mainMenuFrame = setupMainMenuViewFrame();
-        
+        this.mainMenuViewPanel = setupMainMenuViewPanel();
+
     }
 
-    private JFrame setupMainMenuViewFrame() {
+    public JPanel getMainMenuViewPanel() {
+        return mainMenuViewPanel;
+    }
+
+    private JPanel setupMainMenuViewPanel() {
+
+        JPanel mainMenuViewPanel = new JPanel();
+        mainMenuViewPanel.setLayout(new BorderLayout());
+        mainMenuViewPanel.add(mainMenuTitlePanel(), BorderLayout.NORTH);
+        //mainMenuViewPanel.add(mainMenuButtonsPanel(), BorderLayout.CENTER);
+
+        return mainMenuViewPanel;
+    }
+
+    private JPanel mainMenuTitlePanel() {
+        JPanel mainMenuTitlePanel = new JPanel();
+        JLabel mainMenuTitle = new JLabel("Kawaguchi Bank Car Loan Application");
+        mainMenuTitle.setFont(new Font("Arial", Font.PLAIN, 18));
+        mainMenuTitlePanel.add(mainMenuTitle, BorderLayout.CENTER);
+        return mainMenuTitlePanel;
+    }
+
+    private JPanel mainMenuButtonsPanel() {
         return null;
     }
 }
