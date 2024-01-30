b767ea7567f5f2d2bf2ba0801ec9ed7d411dd2f3
iZLikesCS@gmail.com <iZLikesCS@gmail.com>
2024-01-30 18:00:22 +0800

    Modified temporary main to be able to launch GUI of mainMenuView

--- TempMain.java
+++ TempMain.java
@@ -1,7 +1,14 @@
+import javax.swing.*;
+
 public class TempMain {
 
     /** Main used to temporarily work the new MVC implementations. */
     public static void main(String[] args) {
         MainMenuView mainMenuView = new MainMenuView();
+        JFrame test = new JFrame();
+        test.add(mainMenuView.getMainMenuViewPanel());
+        test.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        test.pack();
+        test.setVisible(true);
     }
 }
