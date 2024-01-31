ce772d40865b1596f7e45bb1cebb93644384e611
iZ86 <iZLikesCS@gmail.com>
2024-01-31 15:06:00 +0800

    Added setupCarLoanSchemeTable and carLoanSchemeButtonsPanel method and their starter code and called them in setupCarLoanSchemeViewPanel method

--- CarLoanSchemeView.java
+++ CarLoanSchemeView.java
@@ -4,8 +4,9 @@ import java.awt.*;
 /** This class represents the view for the car loan scheme. */
 public class CarLoanSchemeView {
     // TODO: Implementation after implement the model.
-    private JPanel carLoanSchemeViewPanel;
+    private final JPanel carLoanSchemeViewPanel;
 
+    // Don't know how I'm going to pass my data yet.
     public CarLoanSchemeView() {
         this.carLoanSchemeViewPanel = setupCarLoanSchemeViewPanel();
     }
@@ -16,8 +17,25 @@ public class CarLoanSchemeView {
 
     private JPanel setupCarLoanSchemeViewPanel() {
 
+        // TODO: Constraints.
         JPanel carLoanSchemeViewPanel = new JPanel(new GridBagLayout());
+
+        carLoanSchemeViewPanel.add(setupCarLoanSchemeTable());
+
+        carLoanSchemeViewPanel.add(carLoanSchemeButtonsPanel());
 
         return carLoanSchemeViewPanel;
     }
+
+    private JScrollPane setupCarLoanSchemeTable() {
+        JTable carLoanSchemeTable = new JTable();
+        // TODO: Pass the data that will then be created for the table.
+        return null;
+    }
+
+    private JPanel carLoanSchemeButtonsPanel() {
+        // TODO: Buttons
+        return null;
+    }
+
 }
