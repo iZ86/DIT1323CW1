a0a6816c27b5376b83eb0077f79e53b140389c31
iZ86 <iZLikesCS@gmail.com>
2024-01-31 13:40:49 +0800

    Added startercode for CarLoanSchemeView Class

--- CarLoanSchemeView.java
+++ CarLoanSchemeView.java
@@ -1,5 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
 
 /** This class represents the view for the car loan scheme. */
 public class CarLoanSchemeView {
     // TODO: Implementation after implement the model.
+    private JPanel carLoanSchemeViewPanel;
+
+    public CarLoanSchemeView() {
+        this.carLoanSchemeViewPanel = setupCarLoanSchemeViewPanel();
+    }
+
+    public JPanel getCarLoanSchemeViewPanel() {
+        return carLoanSchemeViewPanel;
+    }
+
+    private JPanel setupCarLoanSchemeViewPanel() {
+
+        JPanel carLoanSchemeViewPanel = new JPanel(new GridBagLayout());
+
+        return carLoanSchemeViewPanel;
+    }
 }
