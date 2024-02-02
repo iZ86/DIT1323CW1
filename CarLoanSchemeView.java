7bb98bb982223ae9b8411409397f117e1ae43ec4
iZ86 <iZLikesCS@gmail.com>
2024-02-02 09:25:32 +0800

    Added constraints for the car loan scheme table

--- CarLoanSchemeView.java
+++ CarLoanSchemeView.java
@@ -6,27 +6,39 @@ public class CarLoanSchemeView {
     // TODO: Implementation after implement the model.
     private final JPanel carLoanSchemeViewPanel;
 
-    // Don't know how I'm going to pass my data yet.
+    /** Creates the view for car loan scheme view.
+     * Takes in data to be shown in the GUI via table.
+     */
     public CarLoanSchemeView(Object[][] tableData, String[] columnNames) {
         this.carLoanSchemeViewPanel = setupCarLoanSchemeViewPanel(tableData, columnNames);
     }
 
+    /** Returns the car loan scheme view panel. */
     public JPanel getCarLoanSchemeViewPanel() {
         return carLoanSchemeViewPanel;
     }
 
+    /** Return a panel that has all the necessary components,
+     * that is composed of the car loan scheme view.
+     */
     private JPanel setupCarLoanSchemeViewPanel(Object[][] tableData, String[] columnNames) {
 
-        // TODO: Constraints.
+        // Initialization
         JPanel carLoanSchemeViewPanel = new JPanel(new GridBagLayout());
 
-        carLoanSchemeViewPanel.add(setupCarLoanSchemeTable(tableData, columnNames));
+        //Setting up properties for car loan scheme table.
+        GridBagConstraints constraintsForCarLoanSchemeViewTable = new GridBagConstraints();
+        constraintsForCarLoanSchemeViewTable.gridy = 0;
+        carLoanSchemeViewPanel.add(setupCarLoanSchemeTable(tableData, columnNames),
+                constraintsForCarLoanSchemeViewTable);
+
 
         //carLoanSchemeViewPanel.add(carLoanSchemeButtonsPanel());
 
         return carLoanSchemeViewPanel;
     }
 
+    /** Returns the car loan scheme table contained in a JScrollPane. */
     private JScrollPane setupCarLoanSchemeTable(Object[][] tableData, String[] columnNames) {
 
         // Table model created so that user cannot edit cells.
