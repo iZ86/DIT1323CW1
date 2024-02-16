522420a1855e7a5ef5479ed3c4899e7c0fba787b
iZ86 <iZLikesCS@gmail.com>
2024-02-16 15:25:53 +0800

    In CarLoanSchemeModel class, did a rework on the variable and data

--- CarLoanSchemeModel.java
+++ CarLoanSchemeModel.java
@@ -2,32 +2,30 @@
 /** This class represents the model that will hold the data
  * of all the car loan scheme information. */
 public class CarLoanSchemeModel {
-    private static final String[] carTypes = new String[]{"Imported", "Local"};
-    private static final String[] importedCarLoanAmounts = new String[]{">300,000", "100,000-300,000", "<100,000"};
-    private static final String[] localCarLoanAmounts = new String[]{">100,000", "50,000-100,000", "<50,000"};
-    private static final String[] importedInterestRates = new String[]{"2.35%", "2.55%", "2.75%"};
-    private static final String[] localInterestRates = new String[]{"3.0%", "3.1%", "3.2%"};
 
-    /** Returns an array of car types. */
-    public static String[] getCarTypes() {
-        return carTypes;
+
+
+    private final String[] carLoanTableDataColumn= {"Car Type","Loan Amount (RM)","Interest Rate (%)"};
+
+    private final String[][] carLoanTableData = {
+            {"Imported",">300,000","2.35%"},
+            {null,"100,000-300,000","2.55%"},
+            {null,"<100,000","2.55%"},
+            {"Local",">100,000","3.0%"},
+            {null,"50,000-100,000","3.1%"},
+            {null,"<50,000","3.2%"},
+    };
+
+    /** Return String[] carLoanTableDataColumn. */
+    public String[] getCarLoanTableDataColumn() {
+        return carLoanTableDataColumn;
     }
 
-    /** Returns an array of imported car loan amounts. */
-    public static String[] getImportedCarLoanAmounts() {
-        return importedCarLoanAmounts;
+    /** Return String[][] carLoanTableData. */
+    public String[][] getCarLoanTableData() {
+        return carLoanTableData;
     }
 
-    public static String[] getLocalCarLoanAmounts() {
-        return localCarLoanAmounts;
-    }
 
-    public static String[] getImportedInterestRates() {
-        return importedInterestRates;
-    }
-
-    public static String[] getLocalInterestRates() {
-        return localInterestRates;
-    }
 
 }
