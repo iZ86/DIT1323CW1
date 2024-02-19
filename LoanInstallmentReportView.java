33df57cf21b7843fa8a5c910c05c184082ae0afa
iZ86 <iZLikesCS@gmail.com>
2024-02-19 09:39:50 +0800

    Made all the variables private, remove the model data, added CarLoanCalculationModel as a variable and in constructor parameter, remove updateLoanInstallmentReportView method and added updateView() method, modified setupShowLoanDataPanel to use the getter methods in carLoanCalculationModel

--- LoanInstallmentReportView.java
+++ LoanInstallmentReportView.java
@@ -4,26 +4,33 @@ import java.awt.event.ActionListener;
 
 public class LoanInstallmentReportView {
     /** JPanel that contains all the component in LoanInstallmentReportView. */
-    private JPanel loanInstallmentReportViewPanel = new JPanel(new GridBagLayout());
-    /** Type of car loaned. */
-    private String carType;
-    /** Loan data. */
-    private double loanTerm, loanAmount, interestRate , outstandingLoanAmount, monthlyRepayment;
-    /** Loan insurance. */
-    private String loanInsuranceStatus;
+    private final JPanel loanInstallmentReportViewPanel = new JPanel(new GridBagLayout());
+    /** Model for LoanInstallReportView. */
+    private final CarLoanCalculationModel carLoanCalculationModel;
     /** Button that changes the view to the MainMenuView. */
-    private JButton backToMainMenuButton = new JButton("Back to main menu");
+    private final JButton backToMainMenuButton = new JButton("Back to main menu");
     /** Button that changes the view to CarLoanInstallmentCalculatorView. */
-    private JButton calculateCarLoanInstallmentButton = new JButton("Calculate new car loan installment");
+    private final JButton calculateCarLoanInstallmentButton = new JButton("Calculate new car loan installment");
 
     /** Constructor to set up the loanInstallmentReportViewPanel. */
-    public LoanInstallmentReportView() {
+    public LoanInstallmentReportView(CarLoanCalculationModel carLoanCalculationModel) {
+        this.carLoanCalculationModel = carLoanCalculationModel;
         setupLoanInstallmentReportViewPanel();
     }
 
     /** Return JPanel loanInstallmentReportViewPanel. */
     public JPanel getLoanInstallmentReportViewPanel() {
         return loanInstallmentReportViewPanel;
+    }
+
+    /** Update JPanel loanInstallmentReportViewPanel. */
+    public void updateView() {
+
+        // Clears the view by removing all components.
+        clearView();
+
+        // Set up JPanel loanInstallmentReportViewPanel again.
+        setupLoanInstallmentReportViewPanel();
     }
 
     /** Add ActionListener listenForBackToMainMenuButton to JButton backToMainMenuButton. */
@@ -34,26 +41,6 @@ public class LoanInstallmentReportView {
     /** Adds an ActionListener to JButton calculateCarLoanInstallmentButton. */
     public void addCalculateCarLoanInstallmentButtonListener(ActionListener listenForCalculateCarLoanInstallmentButton) {
         calculateCarLoanInstallmentButton.addActionListener(listenForCalculateCarLoanInstallmentButton);
-    }
-
-    /** Update JPanel LoanInstallmentReportView. */
-    public void updateLoanInstallmentReportView(String carType, double loanTerm, double loanAmount, double interestRate, double outstandingLoanAmount, double monthlyRepayment, String loanInsuranceStatus) {
-
-        // Assigning new values.
-        this.carType = carType;
-        this.loanTerm = loanTerm;
-        this.loanAmount = loanAmount;
-        this.interestRate = interestRate;
-        this.outstandingLoanAmount = outstandingLoanAmount;
-        this.monthlyRepayment = monthlyRepayment;
-        this.loanInsuranceStatus = loanInsuranceStatus;
-
-        // Reset the view.
-        resetView();
-
-        // Updating the view.
-        this.loanInstallmentReportViewPanel.revalidate();
-        this.loanInstallmentReportViewPanel.repaint();
     }
 
 
@@ -67,32 +54,22 @@ public class LoanInstallmentReportView {
 
         // Setting up the configuration for JPanel titlePanel.
         GridBagConstraints constraintsForTitlePanel = new GridBagConstraints();
-        constraintsForTitlePanel.gridy = 0; // Position of component is first in Y order.
+        constraintsForTitlePanel.gridy = 0; // Position is first in Y order.
 
         // Setting up the configuration for JPanel showLoanDataPanel.
         GridBagConstraints constraintsForShowLoanDataPanel = new GridBagConstraints();
-        constraintsForShowLoanDataPanel.gridy = 1; // Position of component is second in Y order.
+        constraintsForShowLoanDataPanel.gridy = 1; // Position is second in Y order.
         constraintsForShowLoanDataPanel.insets = new Insets(30, 0, 70, 0); // Add gap below and above this component.
 
         // Setting up the configuration for JPanel buttonsPanel.
         GridBagConstraints constraintsForButtonsPanel = new GridBagConstraints();
-        constraintsForButtonsPanel.gridy = 2; // Position of component is third in Y order.
+        constraintsForButtonsPanel.gridy = 2; // Position is third in Y order.
         constraintsForButtonsPanel.insets = new Insets(100, 0, 0, 0); // Add gap above this component.
 
         // Add the JPanels to JPanel viewPanel with their configurations.
         loanInstallmentReportViewPanel.add(titlePanel, constraintsForTitlePanel);
         loanInstallmentReportViewPanel.add(showLoanDataPanel, constraintsForShowLoanDataPanel);
         loanInstallmentReportViewPanel.add(buttonsPanel, constraintsForButtonsPanel);
-    }
-
-    /** Resets JPanel loanInstallmentReportView. */
-    private void resetView() {
-
-        // Clears the view by removing all components.
-        clearView();
-
-        // Setup again.
-        setupLoanInstallmentReportViewPanel();
     }
 
     /** Clear view by removing every component in JPanel loanInstallmentReportView. */
@@ -116,20 +93,18 @@ public class LoanInstallmentReportView {
         return loanInstallmentReportTitlePanel;
     }
 
-    /** Returns a JPanel that contains all the data of the loan. */
+    /** Returns a JPanel that presents all the data of the loan. */
     private JPanel setupShowLoanDataPanel() {
-        // TODO: to update this. use a public method to update, and then .revalidate and .repaint.
-        // TODO: Might need to put some stuff into array to reduce redundancy???
 
         // Initialization.
         JPanel showLoanDataPanel = new JPanel(new GridBagLayout());
-        JLabel showCarType = new JLabel("Car Type: " + this.carType);
-        JLabel showLoanTerm = new JLabel("Loan Term (Years): " + this.loanTerm);
-        JLabel showLoanAmount = new JLabel("Loan Amount (RM): " + this.loanAmount);
-        JLabel showInterestRate = new JLabel("Interest Rate (%): " + this.interestRate);
-        JLabel showOutstandingLoanAmount = new JLabel("Total Outstanding Loan Amount (RM): " + this.outstandingLoanAmount);
-        JLabel showMonthlyRepayment = new JLabel("Monthly Repayment (RM): " + this.monthlyRepayment);
-        JLabel showLoanInstallmentInsurance = new JLabel("Loan Insurance Status: " + this.loanInsuranceStatus);
+        JLabel showCarType = new JLabel("Car Type: " + carLoanCalculationModel.getCarType());
+        JLabel showLoanTerm = new JLabel("Loan Term (Years): " + carLoanCalculationModel.getLoanTerm());
+        JLabel showLoanAmount = new JLabel("Loan Amount (RM): " + carLoanCalculationModel.getLoanAmount());
+        JLabel showInterestRate = new JLabel("Interest Rate (%): " + carLoanCalculationModel.getInterestRatePercentage());
+        JLabel showOutstandingLoanAmount = new JLabel("Total Outstanding Loan Amount (RM): " + carLoanCalculationModel.getOutstandingLoanAmount());
+        JLabel showMonthlyRepayment = new JLabel("Monthly Repayment (RM): " + carLoanCalculationModel.getMonthlyRepayment());
+        JLabel showLoanInstallmentInsurance = new JLabel("Loan Insurance Status: " + carLoanCalculationModel.getLoanInsuranceStatus());
 
         // Setting up configuration for JLabel showCarType.
         GridBagConstraints constraintsForShowCarType = new GridBagConstraints();
@@ -160,7 +135,7 @@ public class LoanInstallmentReportView {
         constraintsForShowLoanInstallmentInsurance.gridy = 6; // Position is seventh in Y order.
 
         // Add the JLabels into the JPanel with their configurations.
-        showLoanDataPanel.add(showCarType,constraintsForShowCarType);
+        showLoanDataPanel.add(showCarType, constraintsForShowCarType);
         showLoanDataPanel.add(showLoanTerm, constraintsForShowLoanTerm);
         showLoanDataPanel.add(showLoanAmount, constraintsForShowLoanAmount);
         showLoanDataPanel.add(showInterestRate, constraintsForShowInterestRate);
@@ -171,7 +146,7 @@ public class LoanInstallmentReportView {
         return showLoanDataPanel;
     }
 
-    /** Returns a JPanel that contains all the buttons in LoanInstallmentReportView. */
+    /** Return a JPanel that contains all the buttons in LoanInstallmentReportView. */
     private JPanel setupLoanInstallmentReportButtonsPanel() {
 
         // Initialization.
@@ -181,20 +156,19 @@ public class LoanInstallmentReportView {
 
         // Setting up configuration for JButton backToMainMenuButton.
         GridBagConstraints constraintsForBackToMainMenuButton = new GridBagConstraints();
-        constraintsForBackToMainMenuButton.gridx = 0; // Position of button is first in X order.
+        constraintsForBackToMainMenuButton.gridx = 0; // Position is first in X order.
         constraintsForBackToMainMenuButton.ipady = heightOfButton; // Vertical height of button.
         constraintsForBackToMainMenuButton.insets = new Insets(0, 0, 0, gapXBetweenButtons); // Add gap on the right.
 
         //Setting up configuration for JButton calculateCarLoanInstallmentButton.
         GridBagConstraints constraintsForCalculateCarLoanInstallmentButton = new GridBagConstraints();
-        constraintsForCalculateCarLoanInstallmentButton.gridx = 1; // Position of button is second in X order.
+        constraintsForCalculateCarLoanInstallmentButton.gridx = 1; // Position is second in X order.
         constraintsForCalculateCarLoanInstallmentButton.fill = GridBagConstraints.VERTICAL; // Make vertical height of this button same as the first button of X order.
 
         // Add the JButtons to the JPanel loanInstallmentReportButtonsPanel with their configurations.
         loanInstallmentReportButtonsPanel.add(backToMainMenuButton, constraintsForBackToMainMenuButton);
         loanInstallmentReportButtonsPanel.add(calculateCarLoanInstallmentButton, constraintsForCalculateCarLoanInstallmentButton);
 
-        // Return the JPanel.
         return loanInstallmentReportButtonsPanel;
     }
 
