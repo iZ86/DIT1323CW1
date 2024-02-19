9100f1d9eb46a7908b8d179e5dc81b008a5f6bf5
iZ86 <iZLikesCS@gmail.com>
2024-02-19 19:25:24 +0800

    Tidy up code, and added comment for class

--- CarLoanSchemeModel.java
+++ CarLoanSchemeModel.java
@@ -1,19 +1,19 @@
 
-/** This class represents the model that will hold the data
- * of all the car loan scheme information. */
+/** This class represents the model for CarLoanSchemeView,
+ *  that will hold the data
+ * of all the car loan scheme information.
+ */
 public class CarLoanSchemeModel {
-
-
-
-    private final String[] carLoanTableDataColumn= {"Car Type","Loan Amount (RM)","Interest Rate (%)"};
-
+    /** Column for car loan scheme table. */
+    private final String[] carLoanTableDataColumn = {"Car Type", "Loan Amount (RM)", "Interest Rate (%)"};
+    /** Data for car loan scheme table. */
     private final String[][] carLoanTableData = {
-            {"Imported",">300,000","2.35%"},
-            {null,"100,000-300,000","2.55%"},
-            {null,"<100,000","2.75%"},
-            {"Local",">100,000","3.0%"},
-            {null,"50,000-100,000","3.1%"},
-            {null,"<50,000","3.2%"},
+            {"Imported", ">300,000", "2.35%"},
+            {null, "100,000-300,000", "2.55%"},
+            {null, "<100,000", "2.75%"},
+            {"Local", ">100,000", "3.0%"},
+            {null, "50,000-100,000", "3.1%"},
+            {null, "<50,000", "3.2%"},
     };
 
     /** Return String[] carLoanTableDataColumn. */
