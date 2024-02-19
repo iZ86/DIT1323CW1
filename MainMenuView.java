393ca6afb53a277d834eee49da61428788d82e8f
iZ86 <iZLikesCS@gmail.com>
2024-02-19 21:12:32 +0800

    Made mainMenuViewPanel initialize before constructor, modified setupMainMenuViewPanel

--- MainMenuView.java
+++ MainMenuView.java
@@ -2,11 +2,11 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
-/** This class represents the kawaguchi bank car loan main menu. */
+/** This class represents the main menu view in the GUI. */
 public class MainMenuView {
 
     /** The main menu view. */
-    private final JPanel mainMenuViewPanel;
+    private final JPanel mainMenuViewPanel = new JPanel(new GridBagLayout());
     /** Button that changes the view to the car loan scheme view. */
     private final JButton displayCarLoanSchemeButton = new JButton("Display Car Loan Scheme");
     /** Button that changes the view to calculate car loan installment view. */
@@ -19,8 +19,7 @@ public class MainMenuView {
 
     /** Constructor is used to set up the main menu view. */
     public MainMenuView() {
-        this.mainMenuViewPanel = setupMainMenuViewPanel();
-
+        setupMainMenuViewPanel();
     }
 
     /** Return JPanel mainMenuViewPanel. */
@@ -28,7 +27,6 @@ public class MainMenuView {
         return mainMenuViewPanel;
     }
 
-    // TODO: Specify the name of the comments for the three addListener methods.
     /** Adds an ActionListener to JButton displayCarLoanSchemeButton. */
     public void addDisplayCarLoanSchemeButtonListener(ActionListener listenForDisplayCarLoanSchemeButton) {
         displayCarLoanSchemeButton.addActionListener(listenForDisplayCarLoanSchemeButton);
@@ -49,29 +47,25 @@ public class MainMenuView {
         exitButton.addActionListener(listenForExitButton);
     }
 
-    /** Return a JPanel that has all the necessary components needed to make the main menu view. */
-    private JPanel setupMainMenuViewPanel() {
+    /** Set up JPanel mainMenuView. */
+    private void setupMainMenuViewPanel() {
 
         // Initialization
-        JPanel viewPanel = new JPanel(new GridBagLayout());
-        JPanel titlePanel = setupMainMenuTitlePanel(); // Main menu title.
-        JPanel buttonsPanel = setupMainMenuButtonsPanel(); // Buttons in main menu.
+        JPanel titlePanel = setupMainMenuTitlePanel();
+        JPanel buttonsPanel = setupMainMenuButtonsPanel();
 
         // Setting up configuration for JPanel titlePanel.
         GridBagConstraints constraintsForTitlePanel = new GridBagConstraints();
-        constraintsForTitlePanel.gridy = 0; // Position of component is first in Y order.
-        constraintsForTitlePanel.insets = new Insets(0, 0, 50, 0); // Gap below the title.
+        constraintsForTitlePanel.gridy = 0; // Position is first in Y order.
+        constraintsForTitlePanel.insets = new Insets(0, 0, 50, 0); // Add gap below.
 
         // Setting up configuration for JPanel buttonsPanel.
         GridBagConstraints constraintsForButtonsPanel = new GridBagConstraints();
-        constraintsForButtonsPanel.gridy = 1; // Position of component is second in Y order.
+        constraintsForButtonsPanel.gridy = 1; // Position is second in Y order.
 
-        // Add the JPanels to JPanel viewPanel with their configurations.
-        viewPanel.add(titlePanel, constraintsForTitlePanel);
-        viewPanel.add(buttonsPanel, constraintsForButtonsPanel);
-
-        // Returns the viewPanel.
-        return viewPanel;
+        // Add the JPanels to JPanel mainMenuViewPanel with their configurations.
+        mainMenuViewPanel.add(titlePanel, constraintsForTitlePanel);
+        mainMenuViewPanel.add(buttonsPanel, constraintsForButtonsPanel);
     }
 
     /** Return a JPanel that contains the title for the main menu. */
@@ -79,7 +73,7 @@ public class MainMenuView {
 
         // Initialization
         JPanel mainMenuTitlePanel = new JPanel(new GridBagLayout());
-        JLabel mainMenuTitle = new JLabel("Kawaguchi Bank Car Loan Application"); // Main menu title.
+        JLabel mainMenuTitle = new JLabel("Kawaguchi Bank Car Loan Application");
 
         // Configuring JLabel mainMenuTitle.
         mainMenuTitle.setFont(new Font("Arial", Font.PLAIN, 18)); // Setting font and size.
@@ -87,7 +81,6 @@ public class MainMenuView {
         // Add JLabel mainMenuTitle to JPanel mainMenuTitlePanel.
         mainMenuTitlePanel.add(mainMenuTitle);
 
-        // Return JPanel mainMenuTitlePanel.
         return mainMenuTitlePanel;
     }
 
@@ -104,29 +97,29 @@ public class MainMenuView {
 
         // Setting up configuration for JButton displayCarLoanSchemeButton.
         GridBagConstraints constraintsForDisplayCarLoanSchemeButton = new GridBagConstraints();
-        constraintsForDisplayCarLoanSchemeButton.gridy = 0; // Position of button is first in Y order.
-        constraintsForDisplayCarLoanSchemeButton.ipady = verticalSizeOfButton; // Vertical size of button.
-        constraintsForDisplayCarLoanSchemeButton.fill = GridBagConstraints.HORIZONTAL; // Match the second button's horizontal size.
+        constraintsForDisplayCarLoanSchemeButton.gridy = 0; // Position is first in Y order.
+        constraintsForDisplayCarLoanSchemeButton.ipady = verticalSizeOfButton; // Vertical size.
+        constraintsForDisplayCarLoanSchemeButton.fill = GridBagConstraints.HORIZONTAL; // Horizontal size is same as the second button in Y order.
 
         // Setting up configuration for JButton calculateCarLoanInstallmentButton.
         GridBagConstraints constraintsForCalculateCarLoanInstallmentButton = new GridBagConstraints();
-        constraintsForCalculateCarLoanInstallmentButton.gridy = 1; // Position of button is second in Y order.
-        constraintsForCalculateCarLoanInstallmentButton.ipadx = horizontalSizeOfButton; // Horizontal size of the button.
+        constraintsForCalculateCarLoanInstallmentButton.gridy = 1; // Position is second in Y order.
+        constraintsForCalculateCarLoanInstallmentButton.ipadx = horizontalSizeOfButton; // Horizontal size.
         constraintsForCalculateCarLoanInstallmentButton.ipady = verticalSizeOfButton; // Vertical size of button.
-        constraintsForCalculateCarLoanInstallmentButton.insets = new Insets(gapYBetweenButtons, 0, gapYBetweenButtons, 0); // Gap above and below this button.
+        constraintsForCalculateCarLoanInstallmentButton.insets = new Insets(gapYBetweenButtons, 0, gapYBetweenButtons, 0); // Gap above and below.
 
         // Setting up configuration for JButton generateSummaryReportButton.
         GridBagConstraints constraintsForGenerateSummaryReportButton = new GridBagConstraints();
-        constraintsForGenerateSummaryReportButton.gridy = 2; // Position of button is third in Y order.
-        constraintsForGenerateSummaryReportButton.ipady = verticalSizeOfButton; // Vertical size of button.
-        constraintsForGenerateSummaryReportButton.fill = GridBagConstraints.HORIZONTAL; // Match the second button's horizontal size.
+        constraintsForGenerateSummaryReportButton.gridy = 2; // Position is third in Y order.
+        constraintsForGenerateSummaryReportButton.ipady = verticalSizeOfButton; // Vertical.
+        constraintsForGenerateSummaryReportButton.fill = GridBagConstraints.HORIZONTAL; // Horizontal size is same as the second button in Y order.
 
         // Setting up configuration for JButton exitButton.
         GridBagConstraints constraintsForExitButton = new GridBagConstraints();
-        constraintsForExitButton.gridy = 3; // Position of button is fourth in Y order.
-        constraintsForExitButton.ipadx = horizontalSizeOfExitButton; // Horizontal size of the EXIT button.
-        constraintsForExitButton.ipady = verticalSizeOfButton; // Vertical size of the button.
-        constraintsForExitButton.insets = new Insets(gapYBetweenButtons, 0, 0, 0); // Gap above the button.
+        constraintsForExitButton.gridy = 3; // Position is fourth in Y order.
+        constraintsForExitButton.ipadx = horizontalSizeOfExitButton; // Horizontal size.
+        constraintsForExitButton.ipady = verticalSizeOfButton; // Vertical size.
+        constraintsForExitButton.insets = new Insets(gapYBetweenButtons, 0, 0, 0); // Add Gap above.
 
         // Add the JButtons to JPanel mainMenuButtonsPanel with their configurations.
         mainMenuButtonsPanel.add(displayCarLoanSchemeButton, constraintsForDisplayCarLoanSchemeButton);
@@ -134,7 +127,6 @@ public class MainMenuView {
         mainMenuButtonsPanel.add(generateSummaryReportButton, constraintsForGenerateSummaryReportButton);
         mainMenuButtonsPanel.add(exitButton, constraintsForExitButton);
 
-        // Return JPanel mainMenuButtonsPanel.
         return mainMenuButtonsPanel;
     }
 }
