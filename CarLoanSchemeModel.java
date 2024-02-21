6e98c756131e785a837ea75a6114b1e3c25679f5
iZ86 <iZLikesCS@gmail.com>
2024-02-21 16:23:12 +0800

    Added decimal places

--- CarLoanSchemeModel.java
+++ CarLoanSchemeModel.java
@@ -8,12 +8,12 @@ public class CarLoanSchemeModel {
     private final String[] carLoanTableDataColumn = {"Car Type", "Loan Amount (RM)", "Interest Rate (%)"};
     /** Data for car loan scheme table. */
     private final String[][] carLoanTableData = {
-            {"Imported", ">300,000", "2.35%"},
-            {null, "100,000-300,000", "2.55%"},
-            {null, "<100,000", "2.75%"},
-            {"Local", ">100,000", "3.0%"},
-            {null, "50,000-100,000", "3.1%"},
-            {null, "<50,000", "3.2%"},
+            {"Imported", ">300,000.00", "2.35%"},
+            {null, "100,000.00-300,000.00", "2.55%"},
+            {null, "<100,000.00", "2.75%"},
+            {"Local", ">100,000.00", "3.00%"},
+            {null, "50,000.00-100,000.00", "3.10%"},
+            {null, "<50,000.00", "3.20%"},
     };
 
     /** Return String[] carLoanTableDataColumn. */
